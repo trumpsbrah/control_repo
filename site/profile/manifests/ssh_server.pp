e9db7277efff57f105bc94992104bcadf1e7e91e
trumpsbrah <68124205+trumpsbrah@users.noreply.github.com>
2020-07-10 14:48:06 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDoQwqYJ2BgdMhFEH6n3WEE46zbB8OVar9kAHGe3GJOs25yWDUnmwFzq9p1ARyUb2veEVocU6o87M1RBajIO5/ndkKuQuYNr669pKS0JLpq1+OYniPok3Q+d3XUralYy3ZkcNPLFE0pMLXxPHIxfNQZESUn9PUKhv2MV+mafhqk3W0PgW0aqREw3BJ8cxajHhanaem03L6VHoY5SryfBduJWJ+TR9PJl5rHVcErLOZmeVcsLTKQazDdl9wexQqMozYwZB9VrJZN5kIsROWjQ5garDeUOQ9tXYIVqQxY/+olWkX2nKMzfg4zqFzME8sbAm4hUe2FCcebNGUGDASTakO3',
 	}  
 }
