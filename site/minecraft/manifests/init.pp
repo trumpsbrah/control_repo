c41db0ebe304b5b5446b0d0a344acfe6bf01f70b
trumpsbrah <68124205+trumpsbrah@users.noreply.github.com>
2020-07-10 15:54:40 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -26,6 +26,6 @@ class minecraft (
   service {'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['java'],File['/opt/minecraft/eula.txt''],File['/etc/systemd/system/minecraft.service']],
+    require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']],
   }
 }
