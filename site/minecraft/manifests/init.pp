1e290eeef92e74b08704ee40ed223e527d2d847a
trumpsbrah <68124205+trumpsbrah@users.noreply.github.com>
2020-07-10 15:12:45 -0500

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,21 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+  file {'/opt/minecraft/server.jar':
+    ensure => file,
+    source => https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar',
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+  
+}
